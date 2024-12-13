3817190db8033b7db1f926efd95e174a54b7414e
erfanlabbafi <erfanlab2000@gmail.com>
2024-12-13 21:59:59 +0330

    fix creat new card and craet card when localstorage has index

--- src/script.js
+++ src/script.js
@@ -4,36 +4,78 @@ let addprocudt = document.getElementById('addprocudt');
 const allprodcts = [];
 const allprodct ={};
 const localproduct = localStorage.getItem("names");
+const fathercard =document.getElementById('fathercard');
+
+
+
 if(localproduct){
     const parseproduct = JSON.parse(localproduct)
     allprodcts.push(...parseproduct)
-    document.getElementById('namekala').textContent = allprodcts[3].nameitem;
-    document.getElementById('taxkala').textContent = allprodcts[3].taxitem;
+    document.getElementById('namekala').textContent = allprodcts.reverse()[0].nameitem; 
+    document.getElementById('taxkala').textContent = allprodcts.reverse()[0].taxitem;
+    console.log(parseproduct)
+    parseproduct.forEach(element => {
+        addf(element.nameitem,element.taxitem);
+    });
 
+
+    
+    
 }else{
     console.log('clear localstorage')
-
+    
 }
+
+
+
+function addf(a,b){
+const creatcard = document.createElement('div');
+    creatcard.innerHTML =`
+    <div class=" flex justify-center items-center gap-5 pt-3 ">
+        <div class="  border border-gray-600 flex flex-col justify-center items-center rounded-md shadow-md shadow-gray-500  "> 
+            <img src="/src/images/man.jpg" alt="">
+            <p class=" text-xl font-bold" id="namekala">"${a}"</p>
+            <div class=" flex justify-between px-1 w-full">
+                <p class=" text-md " id="taxkala">${b}</p>
+                <p class=" font-bold text-md ">:قیمت</p>
+            </div>
+            <div class=" flex justify-around w-full py-5 px-0 text-white font-bold">
+                <button class=" bg-slate-500 border border-gray-900 hover:bg-slate-400 transition-all rounded-md p-1">جزییات</button>   
+                <button class="bg-slate-500 border border-gray-900 hover:bg-slate-400 transition-all  rounded-md p-1">افزودن به سبد</button>
+            </div>
+        </div>
+    </div>
+</div>`;
+   return fathercard.appendChild(creatcard);
+}
+
 function addprocudts(){
     const nameproducts = getnameproduct();
     const producttaxs = gettaxproduct();
-if (nameproducts &&  producttaxs){
-    allprodct.nameitem = nameproducts;
-    allprodct.taxitem = producttaxs;
-    allprodcts.push(allprodct)
-    localStorage.setItem("names", JSON.stringify(allprodcts));
-    console.log(allprodct)
-    console.log(allprodcts)
+    
+    
+    if (nameproducts &&  producttaxs){
+        allprodct.nameitem = nameproducts;
+        allprodct.taxitem = producttaxs;
+        allprodcts.push(allprodct)
+        localStorage.setItem("names", JSON.stringify(allprodcts));
+        console.log(allprodct)
+        console.log(allprodcts)
+        addf(allprodct.nameitem,allprodct.taxitem);
+
+
 
 }else{
 alert("ورودی های خود را بررسی کنید!")
 }
 
-  
+
+
+
+
 }
-console.log(allprodcts[1].nameitem)
 
-
+// not need change
 function gettaxproduct(){
   let producttax = Number(document.getElementById('tax').value);
 
@@ -47,7 +89,7 @@ if (Number(producttax) == !isNaN ){
     
 
 }
-
+s
 }
 function getnameproduct(){
   let nameproduct = document.getElementById('nameproduct').value;
@@ -63,12 +105,15 @@ function getnameproduct(){
 }
 
 }
-
-
-
 function showAndhidden(){
     addprocudt.classList.toggle('hidden')
 }
+// 
+
+
+
+
+
 
 
 
