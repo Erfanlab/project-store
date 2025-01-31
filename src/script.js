7b2548f948e28891791aec6cb68cabbd8792463c
erfanlabbafi <erfanlab2000@gmail.com>
2025-01-31 22:27:35 +0330

    i can find index of in array but cant remove self id in array and edit

--- src/script.js
+++ src/script.js
@@ -6,8 +6,6 @@ const localproduct = localStorage.getItem("names");
 const fathercard =document.getElementById('fathercard');
 
 
-
-
 if(localproduct){
     const parseproduct = JSON.parse(localproduct)
     allprodcts.push(...parseproduct)
@@ -32,16 +30,16 @@ if(localproduct){
 function addf(a,b,c){
 const creatcard = document.createElement('div');
     creatcard.innerHTML =`
-    <div class=" flex justify-center items-center gap-5 pt-3 ">
+    <div class=" flex justify-center items-center gap-5 pt-3 cardi">
         <div class="  border border-gray-600 flex flex-col justify-center items-center rounded-md shadow-md shadow-gray-500  "> 
             <img  src="${c}" alt="" id="imgshow">
-            <p class=" text-xl font-bold" id="namekala">"${a}"</p>
+            <p class=" text-xl font-bold namekala" id="namekala">"${a}"</p>
             <div class=" flex justify-between px-1 w-full">
-                <p class=" text-md " id="taxkala">${b}</p>
+                <p class=" text-md taxkala" id="taxkala">${b}</p>
                 <p class=" font-bold text-md ">:قیمت</p>
             </div>
             <div class=" flex justify-around w-full py-5 px-0 text-white font-bold">
-                <button class=" bg-slate-500 border border-gray-900 hover:bg-slate-400 transition-all rounded-md p-1">جزییات</button>   
+                <button class="deletecard bg-slate-500 border border-gray-900 hover:bg-slate-400 transition-all rounded-md p-1">جزییات</button>   
                 <button class="bg-slate-500 border border-gray-900 hover:bg-slate-400 transition-all  rounded-md p-1">افزودن به سبد</button>
             </div>
         </div>
@@ -53,7 +51,7 @@ const creatcard = document.createElement('div');
 function addprocudts(){
     const nameproducts = getnameproduct();
     const producttaxs = gettaxproduct();
-const allprodct ={};
+    const allprodct ={};
 
     const imgwoman = getwomanimg();
  
@@ -139,3 +137,26 @@ function showAndhidden(){
 
 
 
+
+
+function deleteProduct(event) {
+    const button = event.target;
+    const card = button.closest('.cardi');
+    const taxkalaremove = String(card.querySelector('.namekala').textContent);
+    
+    if (card) {
+        // card.remove();
+        // allprodcts.pop();
+        let kibi = allprodcts.findIndex((x)=> x.nameitem)
+        console.log(kibi)
+        console.log(taxkalaremove)
+        localStorage.setItem("names", JSON.stringify(allprodcts));
+
+        }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.deletecard').forEach(button => {
+        button.addEventListener('click', deleteProduct);
+    });
+});
